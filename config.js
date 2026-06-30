f382369c4797b85cdc883479f68db526ff2b3f81
Blue965 <robin.noe.voulgaris@gmail.com>
2026-06-30 13:27:15 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -10,5 +10,5 @@
 //   apiBaseUrl: 'https://giftblox.kdns.fr'
 //   apiBaseUrl: 'http://173.45.67.89:25565'
 window.GIFTBLOX_CONFIG = {
-    apiBaseUrl: 'https://nexusglobal.com:20188'
+    apiBaseUrl: 'http://145.239.65.119:20188'
 };
